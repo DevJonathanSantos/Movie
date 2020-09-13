832cc9e7ea9464d60066c5a8d211b7c50c1766a1
Jonathan Oliveira <jojortd0@gmail.com>
2020-09-12 23:15:49 -0300

    commit inicial

---
+++ CRUD.EF/Models/MvcMovieContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace CRUD.EF.Models
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+                  : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
